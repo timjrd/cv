cb007c6e8312f0f3fcd2a54d22341e966f11ae32
Timothée Jourde <itim.lcf@gmail.com>
2019-11-13 17:22:17 +0100

    attempting to fix firefox mobile scaling issue + reduce body height after scaling

--- css-scale.hs
+++ css-scale.hs
@@ -3,18 +3,16 @@ import System.Environment
 main = do
   [from,to] <- map read <$> getArgs
   putStrLn []
-  putStr $ unlines $ map (mediaQuery to) [from..to]
+  putStr $ unlines $ reverse $ map (mediaQuery to) [from..to]
   putStrLn []
 
 mediaQuery :: Int -> Int -> String
 mediaQuery orig target = concat
   [ "@media "
-  , "(min-width: " ++ show lo ++ "mm) and "
-  , "(max-width: " ++ show hi ++ "mm) { "
+  , "(max-width: " ++ show target ++ "mm) { "
   , "body { "
   , "transform-origin: top left; "
-  , "transform: scale(" ++ show ratio ++ "); }}" ]
+  , "transform: scale(" ++ show ratio ++ "); "
+  , "height: " ++ show ratio ++ "vh; }}" ]
   where
-    lo = target - 1
-    hi = target
-    ratio = fromIntegral (lo-2) / fromIntegral orig
+    ratio = fromIntegral (target-3) / fromIntegral orig
